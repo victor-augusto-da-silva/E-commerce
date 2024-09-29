3e916120e51bb132226c5710d0d77c8ea05e2a33
Victor Augusto da Silva <victor1231967@hotmail.com>
2024-09-29 13:54:18 -0300

    Api com a rota de listagem de produtos

--- app.py
+++ app.py
@@ -35,13 +35,38 @@ def add_product():
 #rota de deletar
 @app.route('/api/products/delete/<int:product_id>',methods=["DELETE"])
 def delete_product(product_id): 
-    #Recupera e delete o produto se valido
+    #Recupera se valido
     product = Product.query.get(product_id)
     if product: 
         db.session.delete(product)
         db.session.commit()
-        return jsonify({"message" : "Product deleted successully"})
-    return jsonify({"message:": "Product not found"}),404
+        return jsonify({"message": "Product deleted successully"})
+    return jsonify({"message": "Product not found"}),404
+
+
+#rota retorna os detalhes do produto
+@app.route('/api/products/<int:product_id>', methods=["GET"])
+def get_products_details(product_id):
+        product = Product.query.get(product_id)
+        if product:
+             return jsonify({
+                  "id": product.id,
+                  "name": product.name,
+                  "price": product.price,
+                  "description": product.description
+             })
+        return jsonify({"message": "Product not fount"}),404
+
+#rota de atualizar
+@app.route('/api/products/update/<int:product_id>',methods=["PUT"])
+def update_product(product_id): 
+    #atualiza se valido
+    product = Product.query.get(product_id)
+    if product: 
+        db.session.delete(product)
+        db.session.commit()
+        return jsonify({"message": "Product deleted successully"})
+    return jsonify({"message": "Product not found"}),404
 
 
 #Rota raíz
